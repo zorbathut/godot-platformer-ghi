f5a85bafd544603a7afaa9ecfc5c1fd002210459
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-09-21 22:38:27 -0700

    Initialize Def and Ghi properly.

---
+++ Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+class Bootstrap : Node
+{
+    public override void _Ready()
+    {
+        base._Ready();
+
+        // Spool up defs system
+        var parser = new Def.Parser();
+        foreach (var fname in Util.GetFilesFromDir("res://defs"))
+        {
+            parser.AddString(Util.GetFileAsString(fname));
+        }
+        parser.Finish();
+
+        Ghi.Environment.Startup();
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+using System.Collections.Generic;
+
+class Util
+{
+    public static IEnumerable<string> GetFilesFromDir(string dirname)
+    {
+        var dir = new Directory();
+        dir.Open(dirname);
+        dir.ListDirBegin(skipNavigational: true);
+        while (true)
+        {
+            string fname = dir.GetNext();
+            if (fname == "")
+            {
+                break;
+            }
+
+            yield return dirname + "/" + fname;
+        }
+    }
+
+    public static string GetFileAsString(string path)
+    {
+        var file = new File();
+        file.Open(path, (int)File.ModeFlags.Read);
+        var result = file.GetAsText();
+        file.Close();
+        return result;
+    }
+}
